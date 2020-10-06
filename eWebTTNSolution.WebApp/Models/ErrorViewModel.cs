284f07ba85db1bb749b2a369951529b25a06d7ed
Cao Hưởng <canockg@gmail.com>
2020-10-06 08:46:14 +0700

    Add project files.

---
+++ eWebTTNSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eWebTTNSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
